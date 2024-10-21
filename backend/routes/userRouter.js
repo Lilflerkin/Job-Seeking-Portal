158cbe43dbdd0e2779b3dba193690b68f0323f9d
Tripti Mahajan <triptimahajan3@gmail.com>
2024-10-21 14:46:08 +0530

    updated application

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { register,login, logout } from '../controllers/userControllers.js';
+import { register,login, logout,getUser } from '../controllers/userControllers.js';
 import {isAuthorized} from '../middlewares/auth.js'
 
 // POST route for user registration
 router.post('/register', register);
 router.post("/login", login);
 router.get("/logout", isAuthorized,logout);
+router.get("/getUser",isAuthorized,getUser);
 export default router;
