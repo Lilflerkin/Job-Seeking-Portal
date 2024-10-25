eb6f05078bcc6948dd5b62f99265bf801f24fb07
Tripti Mahajan <triptimahajan3@gmail.com>
2024-10-25 16:39:22 +0530

    debugging

--- frontend/src/components/Application/myApplications.jsx
+++ frontend/src/components/Application/myApplications.jsx
@@ -74,31 +74,40 @@ const MyApplications = () => {
         <div className="container">
           <h1>My Applications</h1>
           {applications.length <= 0 ? (
-            <h4>No Applications Found</h4>
+            <>
+              {" "}
+              <h4>No Applications Found</h4>{" "}
+            </>
           ) : (
-            applications.map((element) => (
-              <JobSeekerCard
-                element={element}
-                key={element._id}
-                deleteApplication={deleteApplication}
-                openModal={openModal}
-              />
-            ))
+            applications.map((element) => {
+              return (
+                <JobSeekerCard
+                  element={element}
+                  key={element._id}
+                  deleteApplication={deleteApplication}
+                  openModal={openModal}
+                />
+              );
+            })
           )}
         </div>
       ) : (
         <div className="container">
           <h1>Applications From Job Seekers</h1>
           {applications.length <= 0 ? (
-            <h4>No Applications Found</h4>
+            <>
+              <h4>No Applications Found</h4>
+            </>
           ) : (
-            applications.map((element) => (
-              <EmployerCard
-                element={element}
-                key={element._id}
-                openModal={openModal}
-              />
-            ))
+            applications.map((element) => {
+              return (
+                <EmployerCard
+                  element={element}
+                  key={element._id}
+                  openModal={openModal}
+                />
+              );
+            })
           )}
         </div>
       )}
@@ -113,67 +122,71 @@ export default MyApplications;
 
 const JobSeekerCard = ({ element, deleteApplication, openModal }) => {
   return (
-    <div className="job_seeker_card">
-      <div className="detail">
-        <p>
-          <span>Name:</span> {element.name}
-        </p>
-        <p>
-          <span>Email:</span> {element.email}
-        </p>
-        <p>
-          <span>Phone:</span> {element.phone}
-        </p>
-        <p>
-          <span>Address:</span> {element.address}
-        </p>
-        <p>
-          <span>CoverLetter:</span> {element.coverLetter}
-        </p>
+    <>
+      <div className="job_seeker_card">
+        <div className="detail">
+          <p>
+            <span>Name:</span> {element.name}
+          </p>
+          <p>
+            <span>Email:</span> {element.email}
+          </p>
+          <p>
+            <span>Phone:</span> {element.phone}
+          </p>
+          <p>
+            <span>Address:</span> {element.address}
+          </p>
+          <p>
+            <span>CoverLetter:</span> {element.coverLetter}
+          </p>
+        </div>
+        <div className="resume">
+          <img
+            src={element.resume.url}
+            alt="resume"
+            onClick={() => openModal(element.resume.url)}
+          />
+        </div>
+        <div className="btn_area">
+          <button onClick={() => deleteApplication(element._id)}>
+            Delete Application
+          </button>
+        </div>
       </div>
-      <div className="resume">
-        <img
-          src={element.resume.url}
-          alt="resume"
-          onClick={() => openModal(element.resume.url)}
-        />
-      </div>
-      <div className="btn_area">
-        <button onClick={() => deleteApplication(element._id)}>
-          Delete Application
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
 const EmployerCard = ({ element, openModal }) => {
   return (
-    <div className="job_seeker_card">
-      <div className="detail">
-        <p>
-          <span>Name:</span> {element.name}
-        </p>
-        <p>
-          <span>Email:</span> {element.email}
-        </p>
-        <p>
-          <span>Phone:</span> {element.phone}
-        </p>
-        <p>
-          <span>Address:</span> {element.address}
-        </p>
-        <p>
-          <span>CoverLetter:</span> {element.coverLetter}
-        </p>
+    <>
+      <div className="job_seeker_card">
+        <div className="detail">
+          <p>
+            <span>Name:</span> {element.name}
+          </p>
+          <p>
+            <span>Email:</span> {element.email}
+          </p>
+          <p>
+            <span>Phone:</span> {element.phone}
+          </p>
+          <p>
+            <span>Address:</span> {element.address}
+          </p>
+          <p>
+            <span>CoverLetter:</span> {element.coverLetter}
+          </p>
+        </div>
+        <div className="resume">
+          <img
+            src={element.resume.url}
+            alt="resume"
+            onClick={() => openModal(element.resume.url)}
+          />
+        </div>
       </div>
-      <div className="resume">
-        <img
-          src={element.resume.url}
-          alt="resume"
-          onClick={() => openModal(element.resume.url)}
-        />
-      </div>
-    </div>
+    </>
   );
 };
